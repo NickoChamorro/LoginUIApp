6962190c79a55b276af2f12a6d716c01defee874
Nicko <nicko.chamorro@gmail.com>
2023-03-12 01:51:16 -0300

    first commit

---
+++ config/FirebaseConfig.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyA8pO4wnZV2CH-Dcfu5Qh327bb4mmdlDjE",
+    authDomain: "login-expo-firebase.firebaseapp.com",
+    projectId: "login-expo-firebase",
+    storageBucket: "login-expo-firebase.appspot.com",
+    messagingSenderId: "602459036985",
+    appId: "1:602459036985:web:4299816228dfa6b6925ce8"
+};
